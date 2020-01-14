ad390708cee677d10fbb184a9b290017104a287a
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2020-01-14 11:45:59 -0500

    Initial commit

---
+++ gcn_circulars.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import requests
+import smtplib
+from email.mime.text import MIMEText
+import time
+import re
+import os
+
+
+def get_gcn(gcn_number):
+    url = f'https://gcn.gsfc.nasa.gov/gcn3/{gcn_number:3d}.gcn3'
+    gcn = requests.get(url)
+    return gcn
+
+
+def send_email(gcn_text, subject):
+    msg = MIMEText(gcn_text)
+    msg['Subject'] = subject
+    gmail_username = 'gcn.circulars@gmail.com'
+    me = f'GCN Circulars <{gmail_username}>'
+    you = 'gcn-circulars@googlegroups.com'
+    msg['From'] = me
+    msg['To'] = you
+
+    s = smtplib.SMTP_SSL('smtp.gmail.com')
+    s.ehlo()
+    s.login(gmail_username, gmail_password)
+    s.sendmail(me, [you], msg.as_string())
+    s.close()
+
+
+def get_last_gcn_number():
+    with open('last_gcn.txt') as f:
+        last_gcn = int(f.read())
+    return last_gcn
+
+
+def store_last_gcn_number(gcn_number):
+    with open('last_gcn.txt', 'w') as f:
+        f.write(str(gcn_number))
+
+
+def listen():
+    gcn_number = get_last_gcn_number()
+    while True:
+        gcn_number += 1
+        gcn = get_gcn(gcn_number)
+        if gcn.ok:
+            subject = re.search('SUBJECT: (.*)', gcn.text).groups()[0]
+            print('sending GCN', gcn_number)
+            send_email(gcn.text, subject)
+            store_last_gcn_number(gcn_number)
+        time.sleep(3)
+
+
+if __name__ == '__main__':
+    gmail_password = os.environ['GMAIL_PASSWD']
+    listen()
